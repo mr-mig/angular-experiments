a2eeff686b2e501dfc3f1acfa98dfac4d94a9045
Mr_Mig <alexey.migutsky@gmail.com>
2014-07-02 18:15:58 +0300

    Add UserModel

---
+++ ng-browserify-module/user/UserModel.js
@@ -0,0 +1,12 @@
+// there is no angular.module here
+// User is a simple Model constructor
+function User(name, address) {
+	this.name = name;
+	this.address = address;
+}
+
+User.prototype.getUserData = function () {
+	return this.name + ', ' + this.address;
+};
+
+module.exports = User;
