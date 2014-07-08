13aca1c4245eba13a5d15fabd6db9affc8fdee97
Alexey Migutsky <alexey.migutsky@gmail.com>
2014-07-08 15:53:47 +0300

    Make explicit statement regarding the constructor

--- ng-browserify-module/user/index.js
+++ ng-browserify-module/user/index.js
@@ -3,6 +3,9 @@
 // all angular-related stuff encapsulated here
 module.exports = angular.module('user', [])
 	// define angular factory using our factory function
+	// the factory will create and expose a User contructor 
 		.factory('User', function () {
 			return require('./UserModel')
 		});
+	// in case we have a singleton instance we can use the exports directly
+	//	.factory('User', return require('./UserModel'));
