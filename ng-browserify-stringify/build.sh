8a131122170741abdf0121fe0f8544dea9891d49
Mr_Mig <alexey.migutsky@gmail.com>
2014-07-16 00:10:23 +0300

    Add build files

--- ng-browserify-stringify/build.sh
+++ ng-browserify-stringify/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/fish
+#!/bin/bash
 # does not work!
 # see https://github.com/substack/node-browserify/issues/823
 # ../node_modules/.bin/browserify -t stringify bundle.js > bundled.js
